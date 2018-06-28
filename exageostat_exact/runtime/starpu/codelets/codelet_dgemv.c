b1cb5551f917b37e333ae032bc39b184651fa8a5
Sameh M. Abdulah <abdullsm@kw60194.kaust.edu.sa>
2018-06-28 14:47:48 +0300

    add changes to codelet_ddotp and codelet_dgemv codelets

--- exageostat_exact/runtime/starpu/codelets/codelet_dgemv.c
+++ exageostat_exact/runtime/starpu/codelets/codelet_dgemv.c
@@ -122,7 +122,11 @@ int MORSE_MLE_dgemv_Tile_Async(MORSE_desc_t *descA, MORSE_desc_t * descZ, MORSE_
 
         RUNTIME_options_ws_free(&options);
 	RUNTIME_options_finalize(&options, morse);
-        MORSE_TASK_dataflush_all();
+	//MORSE_TASK_dataflush_all();
+        //MORSE_TASK_dataflush_all(); is replaced in the new chameleon by  MORSE_Desc_Flush( DESC, sequence );
+        MORSE_Desc_Flush( descA, sequence );
+        MORSE_Desc_Flush( descZ, sequence );
+        MORSE_Desc_Flush( descZout, sequence );
         MORSE_Sequence_Wait(sequence);
         return MORSE_SUCCESS;
 }
